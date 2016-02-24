6cf86068dcc54bc81b9e729524c0fe37be9a62b7
Fabian Dietrichson <fabiansd1402@gmail.com>
2016-02-24 12:19:26 +0100

    Update processPairs.go

--- processPairs.go
+++ processPairs.go
@@ -12,9 +12,10 @@ import (
 func main() {
 	send_ch := make(chan networkModule.Udp_message)
 	recieve_ch := make(chan networkModule.Udp_message)
+	object := networkModule.Udp_message{Raddr: "broadcast", Data: 0, Length: 0}
 
-	masterPort := 20004
-	slavePort := 20003
+	masterPort := 20011
+	slavePort := 20012
 
 	master := false
 
@@ -30,18 +31,23 @@ func main() {
 	for !master {
 		select {
 		case msg := <-recieve_ch:
+			object.Data = msg.Data //strconv.Itoa
 			fmt.Println("recieving")
-			fmt.Println(msg.Data)
+			fmt.Println(strconv.Itoa(msg.Data)
 			lastRecieve = time.Now()
 		default:
-			if time.Since(lastRecieve).Seconds() > 3.0 {
+			if time.Since(lastRecieve).Seconds() > 1.0 {
 				fmt.Println("Taking ovah as master")
 				master = true
 			}
 		}
 	}
-	//send_ch <- networkModule.Udp_message{Raddr: "terminate", Data: "", Length: 0}
+	//send_ch <- networkModule.Udp_message{Raddr: "", Data: "terminate", Length: 9}
+	object.Raddr = "terminate"
+	send_ch <- object
+	//recieve_ch <- mg
 
+	
 	fmt.Println("masterStart")
 	err = networkModule.Udp_init(masterPort, slavePort, 1024, send_ch, recieve_ch)
 	if err != nil {
@@ -50,14 +56,15 @@ func main() {
 
 	cmd := exec.Command("gnome-terminal", "-x", "go", "run", "processPairs.go")
 	cmd.Run()
-
-	num := 0
-	msg := networkModule.Udp_message{Raddr: "broadcast", Data: "", Length: 0}
+	
+	num := object.Data
+	msg := networkModule.Udp_message{Raddr: "broadcast", Data: object.Data, Length: 0}
 	for {
+		fmt.Println("object data" + object.Data)
 		msg.Data = strconv.Itoa(num)
 		msg.Length = len(msg.Data)
 		send_ch <- msg
-		fmt.Println("sennding")
+		fmt.Println("sennding" + msg.Data)
 		num++
 		time.Sleep(time.Second * 1)
 	}
